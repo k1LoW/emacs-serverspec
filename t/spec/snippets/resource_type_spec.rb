c2d36083fa886ccc5ad13abb2353e414f30ae69d
k1LoW <k1lowxb@gmail.com>
2015-01-19 10:53:52 +0900

    Refactor code

--- t/spec/snippets/resource_type_spec.rb
+++ t/spec/snippets/resource_type_spec.rb
@@ -4,20 +4,20 @@ resource_type_dir = File.join(File.dirname(__FILE__), *%w[.. .. serverspec lib s
 snippets_resource_type_dir = File.join(File.dirname(__FILE__), *%w[.. .. .. snippets text-mode serverspec ResourceTypes])
 
 RSpec.describe 'ResourceType' do
-  Dir::open(resource_type_dir).each {|f|
+  Dir::open(resource_type_dir).each do |f|
     next if f == "." || f == ".." || f == "base.rb" || f == "docker_base.rb"
     resource_type_name = File.basename(f, ".rb")
-    it resource_type_name + " snippets exits" do
+    it "snippet '" + resource_type_name + "' should be exits" do
       expect(File).to exist(File.join(snippets_resource_type_dir, resource_type_name))
     end
-  }
+  end
 
-  Dir::open(snippets_resource_type_dir).each {|f|
+  Dir::open(snippets_resource_type_dir).each do |f|
     next if f == "." || f == ".."
     snippet_name = File.basename(f)
     resource_type_name = snippet_name + '.rb'
-    it resource_type_name + " snippets not exits" do
+    it "snippet '" + resource_type_name + "' should not be exits" do
       expect(File).to exist(File.join(resource_type_dir, resource_type_name))
     end
-  }
+  end
 end
